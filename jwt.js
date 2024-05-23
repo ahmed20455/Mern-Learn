fc48223285f46731d8b45f032f53c2838517ba0f
Ahmed Abdul Faheem <100959005+ahmed20455@users.noreply.github.com>
2024-05-23 23:22:07 +0530

    Added functions to decode and verify the generated token

--- jwt.js
+++ jwt.js
@@ -1,7 +1,33 @@
 const jwt = require('jsonwebtoken');
-const value = {
-  name : "faheem",
-  password : 123456
-}
+const value = "faheem";
+const tok = jwtsign(value)
+jwtdecode(tok);
+jwtverify(tok);
+//Generating the jwt token
+function jwtsign(value){
 const token = jwt.sign(value, "secret");
 console.log(token)
+return token;
+}
+//decoding the token
+function jwtdecode(token){
+  const decoded = jwt.decode(token);
+  if(decoded){
+    console.log("Decoded");
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+// verifying the token
+function jwtverify(token){
+  try{
+    jwt.verify(token,"secret")
+    console.log("Verified");
+    return true;
+  }
+  catch(e){
+    return false;
+  }
+}
