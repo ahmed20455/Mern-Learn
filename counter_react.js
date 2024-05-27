103b533a53c3e080eb79b75325422f231c00ced2
Ahmed Abdul Faheem <100959005+ahmed20455@users.noreply.github.com>
2024-05-27 23:35:53 +0530

    Create counter_react.js

---
+++ counter_react.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() { 
+  const [count, setCount] = useState(0);
+ 
+  return (
+      <div>
+          <CustomButton count= {count} setCount= {setCount}></CustomButton> 
+    </div>
+  )
+}
+function CustomButton(props){
+  function onClickHandler(){
+    props.setCount(props.count+1);
+  } 
+  return <button onClick={onClickHandler}>Count is {props.count}</button>
+}
+export default App
